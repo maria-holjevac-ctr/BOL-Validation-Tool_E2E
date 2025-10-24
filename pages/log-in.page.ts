eb4f675fe6a4c9d2a8845e2f9baea26872ffcd85
“holjevacmaria” <holjevac.maria@gmail.com>
2025-10-24 12:41:30 +0200

    Log in test cases

--- pages/log-in.page.ts
+++ pages/log-in.page.ts
@@ -2,16 +2,22 @@ import { Page, Locator } from "@playwright/test";
 
 export class LogIn {
   readonly page: Page;
+  readonly logInContainer: Locator;
   readonly userNameInput: Locator;
   readonly passwordInput: Locator;
+  readonly showHideBtn: Locator;
   readonly signInButton: Locator;
+  readonly toastMessage: Locator;
   readonly pageTitle: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.logInContainer = page.locator("div").nth(2);
     this.userNameInput = page.getByRole("textbox", { name: "Username" });
     this.passwordInput = page.getByRole("textbox", { name: "Password" });
+    this.showHideBtn = page.locator("button").first();
     this.signInButton = page.getByRole("button", { name: "Sign in" });
+    this.toastMessage = page.locator("div.mantine-Notifications-notification");
     this.pageTitle = page.getByRole("heading", { name: "BOL Documents" });
   }
 
--- tests/log-in.spec.ts
+++ tests/log-in.spec.ts
@@ -1,26 +1,45 @@
 import { test, expect } from "../fixtures/user.fixture";
 
 test.describe("Log into BOL Validation tool", () => {
-  test.use({ user: "maria" });
   test.beforeEach(async ({ page }) => {
     await page.goto("");
   });
+
+  test("Verify UI of Log in container", async ({ logIn }) => {
+    await expect(logIn.logInContainer).toBeVisible();
+    await expect(logIn.logInContainer).toHaveScreenshot("initial-log-in.png");
+  });
+
+  test("Verify that user can show/hide password", async ({ logIn }) => {
+    await logIn.login("maria", "test");
+    await logIn.showHideBtn.click();
+    await expect(logIn.logInContainer).toHaveScreenshot("show-password.png");
+    await logIn.showHideBtn.click();
+    await expect(logIn.logInContainer).toHaveScreenshot("hide-password.png");
+  });
+
+  test("Verify that user cannot log in if missing data", async ({ logIn }) => {
+    await logIn.login("", "");
+    await expect(logIn.logInContainer).toHaveScreenshot("mandatory-msg1.png");
+    await logIn.login("", "test");
+    await expect(logIn.logInContainer).toHaveScreenshot("mandatory-msg2.png");
+    await logIn.login("maria", "");
+    await expect(logIn.logInContainer).toHaveScreenshot("mandatory-msg3.png");
+    await expect(logIn.pageTitle).not.toBeVisible();
+  });
+
   test("Verify that user can log in with valid credentials", async ({
     logIn,
   }) => {
     await logIn.login(process.env.USERNAME, process.env.PASSWORD);
-    await expect(logIn.pageTitle).toBeVisible();
+    await expect(logIn.toastMessage).toHaveScreenshot("success-log-in-msg.png");
   });
+
   test("Verify that user cannot log in with invalid credentials", async ({
     logIn,
   }) => {
     await logIn.login("maria", "test");
+    await expect(logIn.toastMessage).toHaveScreenshot("failed-log-in-msg.png");
     await expect(logIn.pageTitle).not.toBeVisible();
-    //add different assertion
-  });
-  test("Verify that user cannot log in if missing data", async ({ logIn }) => {
-    await logIn.login("maria", "");
-    await expect(logIn.pageTitle).not.toBeVisible();
-    //add different assertion
   });
 });
